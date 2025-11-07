bf660ceada78a6999a7be9c3dd4655be5e824cbb
w3rr0 <konradmateja65@gmail.com>
2025-11-08 00:18:59 +0100

    add connection and Dockerfile

--- backend/app/api/alert.py
+++ backend/app/api/alert.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+import json
 
 router = APIRouter()
 
+class Alert(BaseModel):
+    id: str
+    location: float
+
 @router.get("/send")
-async def read_alert():
-    return "[ALERT]: alert otrzymany"
+async def read_alert(msg: str):
+    data = json.loads(msg)
+    return "Otrzymano zgłoszenie"
+
