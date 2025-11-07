c5d6098f65591d5e1a0651679f81f3de79569b94
Bartłomiej <bartomiejskowron@icloud.com>
2025-11-07 23:33:04 +0100

    implement immersive mode: hide bottom status bar on android

--- user/app/_layout.tsx
+++ user/app/_layout.tsx
@@ -1,6 +1,9 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -12,6 +15,15 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      // Hide navigation bar on Android
+      NavigationBar.setVisibilityAsync('hidden');
+      NavigationBar.setBehaviorAsync('overlay-swipe');
+      NavigationBar.setBackgroundColorAsync('#00000000');
+    }
+  }, []);
 
   return (
     <MapProvider>
