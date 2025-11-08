7f2d388182b477a8836592806a078cfdfed94d79
Szostak21 <pawelszostak21@gmail.com>
2025-11-08 02:37:54 +0100

    blagam niech przejdzie

---
+++ rescuer/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="compas/index"
+        options={{
+          title: "Kompas",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="navigation/index"
+        options={{
+          title: "Nawigacja",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings/index"
+        options={{
+          title: "Ustawienia",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ rescuer/src/hooks/useTouristLocation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const useTouristLocation = () => {
+  return (
+    <View>
+      <Text>useTouristLocation</Text>
+    </View>
+  );
+};
+
+export default useTouristLocation;
